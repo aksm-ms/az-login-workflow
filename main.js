bce2205f7e70dd468523d693cec831e62759089b
Akshaya1509 <39463098+Akshaya1509@users.noreply.github.com>
2020-02-17 14:49:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("in main js")
+var utility = require('utility')
+
+console.log("Running ps script")
+utility.execSync("powershell", "${{github.workspace}}/test.ps1 -cred foo")
+console.log("PS Script complete")
